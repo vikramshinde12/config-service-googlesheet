225404bba94c4fd4b484b8ddbb607c143b740ecb
Vikram Shinde <vik.shinde@gmail.com>
2020-05-21 08:04:41 +0100

    Changed the default port

--- app/app.py
+++ app/app.py
@@ -98,4 +98,4 @@ def handle_exception(error):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8081)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
