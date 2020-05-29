915b0e9bfaa554d593c4f3253e2db17f72fb41bf
vikram <vikram.shinde@morrisonsplc.co.uk>
2020-05-29 11:46:12 +0100

    Added dependency

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,6 +17,7 @@ resource "google_project_service" "repo" {
 
 resource "google_sourcerepo_repository" "repo" {
   name = var.repository_name
+  depends_on = [google_project_service.repo]
 }
 
 #----------------------------------------------------------------------------------------------
@@ -121,11 +122,13 @@ data "google_project" "project" {
 resource "google_project_iam_binding" "binding" {
   members = ["serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"]
   role = "roles/run.admin"
+  depends_on = [google_project_service.build]
 }
 
 resource "google_project_iam_binding" "sa" {
   members = ["serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"]
   role = "roles/iam.serviceAccountUser"
+  depends_on = [google_project_service.build]
 }
 
 
